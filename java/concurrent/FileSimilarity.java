c452626d7d88bf36d5056ff331d723a4d5c189f2
douglasalves0 <douglas.sousa@ccc.ufcg.edu.br>
2024-08-15 17:15:15 -0300

    optimized to 5 seconds

--- java/concurrent/FileSimilarity.java
+++ java/concurrent/FileSimilarity.java
@@ -38,10 +38,24 @@ public class FileSimilarity {
                 String file2 = args[j];
                 List<Long> fingerprint1 = fileFingerprints.get(file1);
                 List<Long> fingerprint2 = fileFingerprints.get(file2);
-                float similarityScore = similarity(fingerprint1, fingerprint2);
-                System.out.println("Similarity between " + file1 + " and " + file2 + ": " + (similarityScore * 100) + "%");
+                Task2 t = new Task2(fingerprint1, fingerprint2);
+                Thread t2 = new Thread(t, "quero-jantar-no-ru");
+                threads.add(t2);
+                tasks2.add(t);
+                t2.start();
             }
         }
+
+        int k = 0;
+        for (int i = 0; i < args.length; i++) {
+            for (int j = i + 1; j < args.length; j++) {
+                threads.get(k).join();
+                float similarityScore = tasks2.get(k).getAns();
+                System.out.println("Similarity between " + args[i] + " and " + args[j] + ": " + (similarityScore * 100) + "%");
+                k++;
+            }
+        }
+        
     }
 
     private static long sum(byte[] buffer, int length) {
@@ -50,20 +64,6 @@ public class FileSimilarity {
             sum += Byte.toUnsignedInt(buffer[i]);
         }
         return sum;
-    }
-
-    private static float similarity(List<Long> base, List<Long> target) {
-        int counter = 0;
-        List<Long> targetCopy = new ArrayList<>(target);
-
-        for (Long value : base) {
-            if (targetCopy.contains(value)) {
-                counter++;
-                targetCopy.remove(value);
-            }
-        }
-
-        return (float) counter / base.size();
     }
 
     public static class Task implements Runnable {
@@ -89,6 +89,45 @@ public class FileSimilarity {
             }catch(Exception e){}
         }
         public List<Long> getAns() {
+            return ans;
+        }
+    }
+
+    public static class Task2 implements Runnable {
+        private final List<Long> base;
+        private final List<Long> target;
+        private float ans;
+        public Task2(List<Long> base, List<Long> target) {
+            this.base = base;
+            this.target = target;
+        }
+        @Override
+        public void run() {
+            try{
+                int counter = 0;
+                List<Long> targetCopy = new ArrayList<>(target);
+                Collections.sort(targetCopy);
+                Collections.sort(base);
+                
+                int l = 0,l1 = 0;
+                
+                while(l < targetCopy.size() && l1 < base.size()) {
+                    if(targetCopy.get(l) == base.get) {
+                        counter++;
+                        l1++,l2++;
+                    }
+                }
+
+                for (Long value : base) {
+                    if (targetCopy.contains(value)) {
+                        counter++;
+                        targetCopy.remove(value);
+                    }
+                }
+                this.ans = (float) counter / base.size();
+            }catch(Exception e){}
+        }
+        public float getAns() {
             return ans;
         }
     }
